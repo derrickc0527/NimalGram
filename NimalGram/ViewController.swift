06e04f5d263a4592c657f974408aa3981b96cbdf
Derrick Chong <Derrick@Derricks-MacBook-Pro.local>
2016-03-17 14:49:56 -0400

    first commit --setting up parse with my app

---
+++ NimalGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NimalGram
+//
+//  Created by Derrick Chong on 3/17/16.
+//  Copyright © 2016 DerrickCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
